20a1b07ebc030c65c5656d2f481b62710c643e6d
David Alvarez <aalvag@live.com>
2022-01-20 16:43:31 -0300

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,52 @@
+import { Routes, Route, Link } from "react-router-dom";
+import "./App.css";
+import Layout from "antd/lib/layout/layout";
+import {
+  Cryptocurrencies,
+  CryptoDetails,
+  Exchanges,
+  Homepage,
+  Navbar,
+  News,
+} from "./components";
+import { Space, Typography } from "antd";
+
+function App() {
+  return (
+    <div className="app">
+      <div className="navbar">
+        <Navbar />
+      </div>
+      <div className="main">
+        <Layout>
+          <div className="routes">
+            <Routes>
+              <Route path="/" element={<Homepage />} />
+              <Route path="/cryptocurrencies" element={<Cryptocurrencies />} />
+              <Route path="/exchanges" element={<Exchanges />} />
+              <Route path="/crypto/:coinId" element={<CryptoDetails />} />
+              <Route path="/news" element={<News />} />
+            </Routes>
+          </div>
+        </Layout>
+        <div className="footer">
+          <Typography.Title
+            level={5}
+            style={{ color: "white", textAlign: "center" }}
+          >
+            Copyright© {new Date().getFullYear()}{" "}
+            <Link to="/">Cryptoverse Inc.</Link> <br />
+            All Rights Reserved.
+          </Typography.Title>
+          <Space>
+            <Link to="/">Home</Link>
+            <Link to="/exchanges">Exchanges</Link>
+            <Link to="/news">News</Link>
+          </Space>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/Navbar.jsx
@@ -0,0 +1,39 @@
+import { Button, Menu, Typography, Avatar } from "antd";
+import {
+  HomeOutlined,
+  MoneyCollectOutlined,
+  BulbOutlined,
+  FundOutlined,
+  MenuOutlined,
+} from "@ant-design/icons";
+import { Link } from "react-router-dom";
+import logo from "../images/cryptocurrency.png";
+
+function Navbar() {
+  return (
+    <div className="nav-container">
+      <div className="logo-container">
+        <Avatar src={logo} size="large" />
+        <Typography.Title level={2}>
+          <Link to="/">Cryptoverse</Link>
+        </Typography.Title>
+      </div>
+      <Menu theme="dark">
+        <Menu.Item icon={<HomeOutlined />}>
+          <Link to="/">Home</Link>
+        </Menu.Item>
+        <Menu.Item icon={<FundOutlined />}>
+          <Link to="/cryptocurrencies">Cryptocurrencies</Link>
+        </Menu.Item>
+        <Menu.Item icon={<MoneyCollectOutlined />}>
+          <Link to="/exchanges">Exchanges</Link>
+        </Menu.Item>
+        <Menu.Item icon={<BulbOutlined />}>
+          <Link to="/news">News</Link>
+        </Menu.Item>
+      </Menu>
+    </div>
+  );
+}
+
+export default Navbar;
